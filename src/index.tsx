d65e1b55a63afc8f2b92b38335d2f3f22d46f6af
abvanliew <ab.vanliew@gmail.com>
2022-08-07 17:22:14 -0400

    refactor to react routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,5 @@
 import { createRoot } from 'react-dom/client';
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
 import Main from 'views/main';
-import { Tab } from 'views/tabs';
 
 const root = createRoot( document.getElementById( 'root' )! );
-root.render( <Main tab={Tab.Home}/> )
-// root.render(
-//   <BrowserRouter>
-//     <Routes>
-//       <Route path="/">
-//         <Route index element={ <Main tab={Tab.Home}/> }/>
-//         <Route path="editor" element={ <Main tab={Tab.CharEditor}/> }/>
-//       </Route>
-//     </Routes>
-//   </BrowserRouter>
-// );
+root.render( <Main/> );
--- src/views/main.tsx
+++ src/views/main.tsx
@@ -1,59 +1,42 @@
-import React, { useState } from 'react';
-import Tabs, { Tab } from 'views/tabs';
-import Home from 'views/home';
-import GameLibrary from 'gameData/library';
-import CharacterEditor from 'charEditor/main';
+import { useState } from 'react';
+import { BrowserRouter, Link, Outlet, Route, Routes } from 'react-router-dom';
+
 import 'styles/index.css';
+import Home from 'views/home';
+import CharacterEditor from 'charEditor/main';
+import GameLibrary from 'gameData/library';
 
 interface AppState {
-  tab: Tab;
   library: GameLibrary;
 }
 
-interface AppProps {
-  tab: Tab;
-}
-
-function Main( { ...props }: AppProps ): JSX.Element {
-  const [ appState, setAppState ] = useState<AppState>({ tab: props.tab } as AppState);
-  loadGameData();
-
-  function loadGameData() {
-    if( appState.library === null || appState.library === undefined ) {
-      setAppState( { ...appState, library: new GameLibrary() } );
-    }
-  }
-
-  function changeTab( e: React.MouseEvent<HTMLDivElement, MouseEvent> ) {
-    let newTab: Tab | undefined = Tabs.find( ( tab ) => { return tab.id === e.currentTarget.id } )?.selector;
-    if( newTab !== undefined && newTab !== appState.tab ) { 
-      setAppState( { ...appState, tab: newTab } ) 
-    }
-  }
-
-  function TabList(): JSX.Element {
-    return (
-      <> { Tabs.map( ( tab ) => { 
-        return <div key={tab.id} id={tab.id} onClick={changeTab}>{tab.name}</div> 
-      } ) } </> 
-    )
-  }
+function Main(): JSX.Element {
+  const [ appState, setAppState ] = useState<AppState>({ library: new GameLibrary() });
 
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route index element={<Home />} />
+          <Route path="editor" element={<CharacterEditor library={ appState.library }/>}>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Body(): JSX.Element {
+  return (
     <div className='grid'>
       <div className='gridHeader'>
-        <TabList/>
+        <Link className='plain' to={"/"}>Home</Link>
+        <Link className='plain' to={"editor"}>Editor</Link>
       </div>
-      <div className='gridMain'> 
-        { 
-          appState.tab === Tab.Home ? <Home/> : 
-          appState.tab === Tab.CharEditor ? <CharacterEditor library={appState.library}/> : 
-          <div>Error</div>
-        } 
+      <div className='gridMain'>
+        <Outlet/>
       </div>
     </div>
-    </>
   )
 }
 
